bbd8afc0e45e0a7ff1fb4d3e605a2b8816de96af
Prajjwal44 <prajjwalsetty@gmail.com>
2026-05-15 00:02:53 +0530

    2nd  milestone completed

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.get("/", (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;wdc2qdws
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
