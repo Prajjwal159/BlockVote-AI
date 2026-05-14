0ee074d2d87c51594bf0d1f8526b8cbbe6ef02ce
Prajjwal44 <prajjwalsetty@gmail.com>
2026-05-14 23:28:22 +0530

    verification working

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const authRoutes = require("./routes/authRoutes");
 
 const connectDB = require("./config/db");
 
@@ -13,6 +14,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
     res.send("BlockVote AI Backend Running");
